158dcd1c2445b4a2c6b2b0a9bd4b48a9d99f4efc
Yared Nebiat <nebiat@usc.edu>
2024-03-12 22:33:20 -0700

    Completed equal paths function

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,46 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int treeDepth(Node* root) {
+    if(root == nullptr) {
+        return 0;
+    }
+    else {
+        int left_d = treeDepth(root->left);
+        int right_d = treeDepth(root->right);
+        int maxDepth = (left_d > right_d) ? left_d : right_d;
 
+        return 1 + maxDepth;
+    }
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+
+    if(root == nullptr) {
+        return true;
+    }
+    
+    int left_d = treeDepth(root->left);
+    int right_d = treeDepth(root->right);
+
+    if(root->left != nullptr && root->right != nullptr) {
+        if (left_d != right_d) {
+            return false;
+        }
+        else {
+            return equalPaths(root->left) && equalPaths(root->right);
+        }
+    }
+    
+    if (root->left != nullptr) {
+        return equalPaths(root->left);
+    }
+    if (root->right != nullptr) {
+        return equalPaths(root->right);
+    }
+
+    return true;
 
 }
 
